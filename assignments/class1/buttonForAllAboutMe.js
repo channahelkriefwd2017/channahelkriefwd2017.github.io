7a2527b8677d3db7963bfd3698783826195df38a
channahelkriefwd2017 <channahelkrief@gmail.com>
2016-11-08 13:43:44 +0200

    my assignmnet

---
+++ assignments/class1/buttonForAllAboutMe.js
@@ -0,0 +1,16 @@
+document.getElementById("myButton").onclick = changeColor;
+var currentColor = "darkmagenta";
+
+function changeColor(){;
+
+    if (currentColor == "darkmagenta"){
+        document.body.style.color = "chartreuse";
+        currentColor = "chartreuse";
+    } else {
+        document.body.style.color = "darkmagenta";
+        currentColor = "darkmagenta";
+
+
+    }
+     return currentColor;
+}
