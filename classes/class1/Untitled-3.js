c4cdd0e94302d5c4cc2c84ff20b56925517267ca
channahelkriefwd2017 <channahelkrief@gmail.com>
2016-11-10 09:55:00 +0200

    renamed

--- classes/class1/Untitled-3.js
+++ classes/class1/Untitled-3.js
@@ -2,15 +2,15 @@ document.getElementById("myButton").onclick = changeColor;
 var currentColor = "aqua";
 
 function changeColor(){
-    
+
     if (currentColor == "aqua"){
         document.body.style.color = "yellow";
         currentColor = "yellow";
     } else {
         document.body.style.color = "aqua";
         currentColor = "aqua";
-       
-      
+
+
     }
      return currentColor;
 }
