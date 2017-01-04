0fb78dbb1c58c4389e31e7ba08cabd80649858bf
channahelkriefwd2017 <channahelkrief@gmail.com>
2017-01-04 09:36:52 +0200

    add

---
+++ assignments/class8/main.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    var $myNavItem = $("li");
+
+    $myNavItem.on("mouseover", function(){
+        $(this).find(".scrollNav").animate(
+            {height: "120px"},
+            {queue: false,
+             duration: 500,
+             easing: "linear",
+             start: function(){
+                 $(this).css("border-width", "3px");
+             }
+            }
+        );
+
+    }).on("mouseleave", function(){
+        $(this).find(".scrollNav").animate(
+            {height: "0px"},
+            {queue: false,
+             duration: 500,
+             easing: "linear",
+             complete: function(){
+                 $(this).css("border-width","0");
+             }
+            }
+
+        );
+    });
+});
