0a5fcf455e0abab50757e993e841c7fcd17749c9
channahelkriefwd2017 <channahelkrief@gmail.com>
2017-02-02 12:38:35 +0200

    add

---
+++ classes/class13/portfolio/index2.php
@@ -0,0 +1,7 @@
+<? $page_title= "Second Page";
+$page = "page2";
+    include("header.php"); ?>
+        <main>
+            <div class="container-fluid link">Hi! This is a second page!!</div>
+        </main>
+<? include("footer.php"); ?>
