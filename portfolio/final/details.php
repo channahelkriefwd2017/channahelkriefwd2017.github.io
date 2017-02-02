570522bd2d944ce074c23511bca3645556e5ed4f
channahelkriefwd2017 <channahelkrief@gmail.com>
2017-02-02 11:06:27 +0200

    add

---
+++ portfolio/final/details.php
@@ -0,0 +1,16 @@
+<? include("header.html"); ?>
+        <main>
+            <div class="container-fluid">
+
+                <div class="row row2">
+                    <div class="col-xs-12"><div class="col-xs-7">Name of Project</div><div class="col-xs-5">Details</div></div>
+
+                </div>
+
+                <div class="row row3">
+                    <div class="col-xs-12"><div class="col-xs-12"><img src="websites/download%20(1).jpg"></div></div>
+                </div>
+
+            </div>
+        </main>
+<? include("footer.html"); ?>
